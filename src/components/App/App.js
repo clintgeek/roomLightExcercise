c20fa7b0afa6efdeef847621a3bb59763caa9fcc
Clint Geek <clint@clintgeek.com>
2020-06-07 19:30:19 -0500

    add useRef and useEffect hooks, add fancy counter and alert

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="container">
       <div className="room">
         <Room name="Test Room" />
+        <div className="encouragement">(Try cycling it 5 or more times!)</div>
       </div>
       <div className="moreInfo">
         <a className="link" href="https://github.com/clintgeek/roomLightExcercise">github.com/clintgeek/roomLightExcercise</a>
--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import LightBulb from '../LightBulb/';
 import LightSwitch from '../LightSwitch';
 import './Room.css';
 
 function Room(props) {
   const [ powered, setPower ] = useState(false);
+  const [ powerOnCount, setPowerOnCount ] = useState(0);
+  const prevCountRef = useRef();
+  const prevCount = prevCountRef.current;
 
-  const togglePower = () => setPower(!powered);
+  const togglePower = () => {
+    setPower(!powered);
+    if (!powered) setPowerOnCount(powerOnCount + 1);
+  };
+
+  const updateTitleBar = () => {
+    document.title = `You turned the light on ${powerOnCount} times`;
+  };
+
+  const callAlert = () => {
+    if (prevCount && 
+        prevCount !== powerOnCount && 
+        powerOnCount % 5 === 0 
+    ){
+      alert('You turned the light on ' + powerOnCount + ' times! You must really like this light!');
+    }
+  };
+
+  useEffect(() => {
+    prevCountRef.current = powerOnCount;
+    updateTitleBar();
+    callAlert();
+  });
 
   var lightLevelClass = powered ? ' bright' : '';
   const roomClass = 'walledRoom' + lightLevelClass;
